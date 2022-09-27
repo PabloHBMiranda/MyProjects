4017d0e677f6e7c06a4f2bd650aa9cedbb454cd2
Pablo Miranda <112266050+PabloHBMiranda@users.noreply.github.com>
2022-09-27 19:41:50 -0300

    "Layout Fragments"

---
+++ Fragments/app/src/main/java/com/example/fragments/Fragment1.java
@@ -0,0 +1,30 @@
+package com.example.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Fragment1 extends Fragment {
+    Button btnFrag1, btnFrag2;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.layout_fragment1,container,false);
+        btnFrag1 = view.findViewById(R.id.btn01);
+        btnFrag2 = view.findViewById(R.id.btn02);
+        btnFrag2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
