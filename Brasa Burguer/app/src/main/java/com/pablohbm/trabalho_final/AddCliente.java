a4f9d920fcd5fc67fa3bdfa7e411417d9cfa4f14
Pablo Miranda <112266050+PabloHBMiranda@users.noreply.github.com>
2022-11-17 15:38:37 -0300

    Versão: 1.0

---
+++ Brasa Burguer/app/src/main/java/com/pablohbm/trabalho_final/AddCliente.java
@@ -0,0 +1,14 @@
+package com.pablohbm.trabalho_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddCliente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_cliente);
+    }
+}
