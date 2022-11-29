3540c20dd321fef78e69cc63ff43a0d13a538029
Pablo Miranda <112266050+PabloHBMiranda@users.noreply.github.com>
2022-11-28 23:08:58 -0300

    Versão 1.2

--- AED/app/src/main/java/com/pablohbm/aed/TelaJogo.java
+++ AED/app/src/main/java/com/pablohbm/aed/TelaJogo.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import java.util.ArrayList;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -13,12 +14,59 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class TelaJogo extends AppCompatActivity {
-    public void TesteLetra(String letra){
-        int i, j;
-        for(i = 0; i < letra.length(); i++){
 
+public class TelaJogo extends AppCompatActivity {
+
+    public int TesteLetra(String letraEditText) {
+        char letra = letraEditText.charAt(0);
+        int possuiLetra = 0;
+        for (i = 0; i < palavraJogada.length(); i++) {
+            if (letra == palavraJogada.charAt(i)) {
+                listaLetras.set(i, letra);
+                possuiLetra = 1;
+            }
         }
+        palavraTela = "";
+        for (i = 0 ; i < listaLetras.size(); i++) {
+            palavraTela += listaLetras.get(i);
+        }
+        return possuiLetra;
+    }
+
+    public int MudaForca(){
+        if(erros == 0){
+            cabeca.setVisibility(View.VISIBLE);
+            erros++;
+        }
+        else{
+            if(erros == 1){
+                tronco.setVisibility(View.VISIBLE);
+                erros++;
+            }
+            else{
+                if(erros == 2){
+                    bdireito.setVisibility(View.VISIBLE);
+                    erros++;
+                }
+                else{
+                    if(erros == 3){
+                        besquerdo.setVisibility(View.VISIBLE);
+                        erros++;
+                    }
+                    else{
+                        if(erros==4){
+                            pesquerdo.setVisibility(View.VISIBLE);
+                            erros++;
+                        }
+                        else{
+                            pdireito.setVisibility(View.VISIBLE);
+                            erros++;
+                        }
+                    }
+                }
+            }
+        }
+        return erros;
     }
 
 
@@ -27,8 +75,11 @@ public class TelaJogo extends AppCompatActivity {
     ListView listaJogo;
     ImageView cabeca, tronco, bdireito, besquerdo, pesquerdo, pdireito;
     TextView palavraJogo;
-    String palavraJogada;
+    String palavraJogada, palavraTela;
     String dicaJogada;
+    int i, erros = 0;
+    ArrayList listaLetras = new ArrayList();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +102,21 @@ public class TelaJogo extends AppCompatActivity {
         pdireito = findViewById(R.id.pdireito);
         //boneco
 
+        for (i = 0; i < palavraJogada.length(); i++) {
+            if (' ' == (palavraJogada.charAt(i))) {
+                listaLetras.add(" ");
+            } else {
+                listaLetras.add("_");
+            }
+
+        }
+
+        for(i = 0; i < listaLetras.size(); i++){
+            palavraTela+=listaLetras.get(i);
+        }
+
+        palavraJogo.setText(palavraTela);
+
         menuJogo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -63,10 +129,17 @@ public class TelaJogo extends AppCompatActivity {
         btnLetra.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!edLetra.getText().equals("")){
-
-                }
-                else{
+                if (!edLetra.getText().equals("")) {
+                    String letraJogada = edLetra.getText().toString();
+                    if(TesteLetra(letraJogada) == 1){
+                        palavraJogo.setText(palavraTela);
+                    }
+                    else{
+                        if(MudaForca() > 5){
+                            Intent i = new Intent(TelaJogo.this, GameOver.class);
+                        }
+                    }
+                } else {
                     Toast.makeText(TelaJogo.this, "Campo Vazio", Toast.LENGTH_SHORT).show();
                 }
             }
