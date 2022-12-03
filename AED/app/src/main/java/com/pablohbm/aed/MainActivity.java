04c2756e4d3b2cad2b498f69d895fd5f97c96015
Pablo Miranda <112266050+PabloHBMiranda@users.noreply.github.com>
2022-12-03 09:42:04 -0300

    Versão 1.3.1

--- AED/app/src/main/java/com/pablohbm/aed/MainActivity.java
+++ AED/app/src/main/java/com/pablohbm/aed/MainActivity.java
@@ -3,13 +3,27 @@ package com.pablohbm.aed;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button jogar1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        jogar1 = findViewById(R.id.jogar1);
+
+        jogar1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, TelaJogo.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
--- AED/app/src/main/java/com/pablohbm/aed/TelaJogo.java
+++ AED/app/src/main/java/com/pablohbm/aed/TelaJogo.java
@@ -5,14 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 
 
 public class TelaJogo extends AppCompatActivity {
@@ -27,38 +31,33 @@ public class TelaJogo extends AppCompatActivity {
             }
         }
         palavraTela = "";
-        for (i = 0 ; i < listaLetras.size(); i++) {
+        for (i = 0; i < listaLetras.size(); i++) {
             palavraTela += listaLetras.get(i);
         }
         return possuiLetra;
     }
 
-    public int MudaForca(){
-        if(erros == 0){
+   public int MudaForca() {
+        if (erros == 0) {
             cabeca.setVisibility(View.VISIBLE);
             erros++;
-        }
-        else{
-            if(erros == 1){
+        } else {
+            if (erros == 1) {
                 tronco.setVisibility(View.VISIBLE);
                 erros++;
-            }
-            else{
-                if(erros == 2){
+            } else {
+                if (erros == 2) {
                     bdireito.setVisibility(View.VISIBLE);
                     erros++;
-                }
-                else{
-                    if(erros == 3){
+                } else {
+                    if (erros == 3) {
                         besquerdo.setVisibility(View.VISIBLE);
                         erros++;
-                    }
-                    else{
-                        if(erros==4){
+                    } else {
+                        if (erros == 4) {
                             pesquerdo.setVisibility(View.VISIBLE);
                             erros++;
-                        }
-                        else{
+                        } else {
                             pdireito.setVisibility(View.VISIBLE);
                             erros++;
                         }
@@ -73,12 +72,15 @@ public class TelaJogo extends AppCompatActivity {
     EditText edLetra, edjogoPalavra;
     Button btnLetra, btnPalavra, menuJogo, jogoDica;
     ListView listaJogo;
-    ImageView cabeca, tronco, bdireito, besquerdo, pesquerdo, pdireito;
+    ImageView cabeca, tronco, bdireito, besquerdo, pesquerdo, pdireito, forca;
     TextView palavraJogo;
-    String palavraJogada, palavraTela;
+    String palavraJogada = "ARROZ", palavraTela="";
     String dicaJogada;
     int i, erros = 0;
     ArrayList listaLetras = new ArrayList();
+    ArrayList jogadasErradas = new ArrayList();
+    ArrayAdapter adapter;
+    String sorteioPalavras[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -93,6 +95,9 @@ public class TelaJogo extends AppCompatActivity {
         listaJogo = findViewById(R.id.listaJogo);
         palavraJogo = findViewById(R.id.palavraJogo);
 
+        adapter = new ArrayAdapter(TelaJogo.this, android.R.layout.simple_list_item_1, jogadasErradas);
+        listaJogo.setAdapter(adapter);
+
         //boneco
         cabeca = findViewById(R.id.cabeca);
         tronco = findViewById(R.id.tronco);
@@ -100,7 +105,15 @@ public class TelaJogo extends AppCompatActivity {
         besquerdo = findViewById(R.id.besquerdo);
         pesquerdo = findViewById(R.id.pesquerdo);
         pdireito = findViewById(R.id.pdireito);
+        forca = findViewById(R.id.forca);
         //boneco
+
+
+       /* Random r = new Random();
+        int x = r.nextInt(10);
+        sorteioPalavras = new String[]
+                {"MOUSE", "CADEIRA", "CABELO", "ANTENA", "OLEO MOTOR", "PAO DE HAMBURGUER", "PERFUME", "AÇAI", "BOMDA DE GASOLINA", "QUADRA DE FUTEBOL"};
+        palavraJogada = sorteioPalavras[x];*/
 
         for (i = 0; i < palavraJogada.length(); i++) {
             if (' ' == (palavraJogada.charAt(i))) {
@@ -111,11 +124,13 @@ public class TelaJogo extends AppCompatActivity {
 
         }
 
-        for(i = 0; i < listaLetras.size(); i++){
-            palavraTela+=listaLetras.get(i);
+        for (i = 0; i < listaLetras.size(); i++) {
+            palavraTela += listaLetras.get(i);
         }
 
         palavraJogo.setText(palavraTela);
+
+
 
         menuJogo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -129,15 +144,18 @@ public class TelaJogo extends AppCompatActivity {
         btnLetra.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!edLetra.getText().equals("")) {
-                    String letraJogada = edLetra.getText().toString();
-                    if(TesteLetra(letraJogada) == 1){
+                if (!edLetra.getText().toString().equals("")) {
+                    String jogouLetra = edLetra.getText().toString().toUpperCase();
+                    if (TesteLetra(jogouLetra) == 1) {
                         palavraJogo.setText(palavraTela);
-                    }
-                    else{
-                        if(MudaForca() > 5){
+                        edLetra.setText("");
+                    } else {
+                        jogadasErradas.add(jogouLetra);
+                        adapter.notifyDataSetChanged();
+                        if (MudaForca() > 5) {
                             Intent i = new Intent(TelaJogo.this, GameOver.class);
                         }
+                        edLetra.setText("");
                     }
                 } else {
                     Toast.makeText(TelaJogo.this, "Campo Vazio", Toast.LENGTH_SHORT).show();
@@ -145,5 +163,24 @@ public class TelaJogo extends AppCompatActivity {
             }
         });
 
+        btnPalavra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!edjogoPalavra.getText().toString().equals("")){
+                    String jogouPalavra = edjogoPalavra.getText().toString().toUpperCase();
+                    if(jogouPalavra.equals(palavraJogada)){
+                        Toast.makeText(TelaJogo.this, "GANHOU!", Toast.LENGTH_SHORT).show();
+                        edjogoPalavra.setText("");
+                    }
+                    else{
+                        Toast.makeText(TelaJogo.this, "PERDEU!", Toast.LENGTH_SHORT).show();
+                        edjogoPalavra.setText("");
+                    }
+                }
+                else{
+                    Toast.makeText(TelaJogo.this, "Campo Vazio", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
